bc7f9d2b33889ec591593ea87e204d5db39e84f1
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-08-05 23:26:50 +0530

    server setup

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,6 +1,7 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
+import { io } from 'socket.io-client';
 
 
 const TOOLBAR_OPTIONS = [
@@ -18,6 +19,13 @@ const TOOLBAR_OPTIONS = [
 
 export default function TextEditor() {
     
+    useEffect( () => {
+       const socket = io("http://localhost:3001")
+
+       return () => {
+        socket.disconnect();
+       }
+    })
     const wrapperRef = useCallback((wrapper) => {
         if(wrapper == null) return
         wrapper.innerHTML=""
