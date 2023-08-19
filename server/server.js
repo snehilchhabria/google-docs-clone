9c55fadfec1e20695454b66d36060bc2ab137270
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-08-19 16:17:36 +0530

    mongoose

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+const mongoose = require("mongoose")
+
+mongoose.connect('mongodb://127.0.0.1/google-docs-clone');
+
 const io = require("socket.io")(3001, {
     cors: {
         origin: "http://localhost:3000",
@@ -10,7 +14,7 @@ io.on("connection", socket => {
         const data = ""
         socket.join(documentId)
         socket.emit("load-document", data)
-        
+
         socket.on("send-changes",delta => {
          socket.broadcast.to(documentId).emit("receive-changes", delta) // so ths broadcasts the changes to evryone else on the server except us
     })
