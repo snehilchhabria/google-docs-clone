1e5a586f15112065edb562abff17dde0fee05769
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-08-20 00:32:28 +0530

    update

--- server/server.js
+++ server/server.js
@@ -12,13 +12,16 @@ const io = require("socket.io")(3001, {
 const defaultValue = ""
 
 io.on("connection", socket => {
-    socket.on('get-document', documentId => {
-        const document = findOrCreateDocument(documentId)
+    socket.on('get-document', async documentId => {
+        const document = await findOrCreateDocument(documentId)
         socket.join(documentId)
         socket.emit("load-document", document.data)
 
         socket.on("send-changes",delta => {
          socket.broadcast.to(documentId).emit("receive-changes", delta) // so ths broadcasts the changes to evryone else on the server except us
+    })
+    socket.on("save-document", async data => {
+        await Document.findByIdAndUpdate(documentId, { data})
     })
     })
 })
